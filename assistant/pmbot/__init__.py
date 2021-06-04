a234d2abd0b46e16df886edde09bf343c6a69d04
Jerryadisaputraa <78988675+jerryadisaputra@users.noreply.github.com>
2021-06-04 17:29:45 +0000

    Update __init__.py

--- assistant/pmbot/__init__.py
+++ assistant/pmbot/__init__.py
@@ -2,5 +2,5 @@ from pyUltroid.functions.asst_fns import *
 from pyUltroid.functions.botchat_db import *
 from .. import *
 
-OWNER_NAME = ultroid_bot.me.first_name
-OWNER_ID = ultroid_bot.me.id
+OWNER_NAME = Jee_bot.me.first_name
+OWNER_ID = Jee_bot.me.id

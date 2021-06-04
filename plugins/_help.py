46515fc2fe27e7ce448b7135e46dad4451b57d6d
Jerryadisaputraa <78988675+jerryadisaputra@users.noreply.github.com>
2021-06-04 17:02:34 +0000

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -26,8 +26,8 @@ async def ult(ult):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP[plug]:
                     output += i
-                output += "\n© @Te
-              "
+                output += "\n© @ALGTRR"
+              
                 await eor(ult, output)
             elif plug in CMD_HELP:
                 kk = f"Plugin Name-{plug}\n\n✘ Commands Available -\n\n"
@@ -39,7 +39,8 @@ async def ult(ult):
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @TeamUltroid"
+                    x += "\n© @ALGTRR"
+                   
                     await eor(ult, x)
                 except BaseException:
                     await eod(ult, get_string("help_1").format(plug), time=5)
